47dc3cd6c3543d0196baef422b9be7e2cf6082f7
Fabian <32894026+fabianhoegger@users.noreply.github.com>
2018-02-16 16:05:38 +0200

    Update editor.js

--- editor.js
+++ editor.js
@@ -7,7 +7,7 @@ var downloadLink;
 function init() {
   // Initialise the map.
   map = new google.maps.Map(document.getElementById('map-holder'), {
-    center: {lat: 39.60983, lng: 19.818926},
+    center: {lat: 39.60983, lng: 19.818900},
     zoom: 9,
     mapTypeControl: false,
     streetViewControl: false,
