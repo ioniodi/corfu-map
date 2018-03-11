732685667182715fdc07d17ae13de53a208008d3
Fabian <32894026+fabianhoegger@users.noreply.github.com>
2018-03-11 14:07:03 +0200

    final version

--- editor.js
+++ editor.js
@@ -5,7 +5,7 @@ var dropContainer;
 var panel;
 var geoJsonInput;
 var downloadLink;
-
+var Color="";
 
 //var deleteFlag=0;
 //var mouseLocation;
@@ -13,7 +13,7 @@ function init() {
   // Initialise the map.
   map = new google.maps.Map(document.getElementById('map-holder'), {
     center: {lat: 39.624107, lng: 19.922318},
-    zoom: 16,
+    zoom: 17,
     mapTypeControl: false,
     streetViewControl: false,
     fullscreenControl: false,
@@ -27,6 +27,25 @@ function init() {
 
   });
   bindDataLayerListeners(map.data);
+  map.data.setStyle({
+    strokeColor: '#FFFFFF',
+    strokeWeight: 3
+  });
+
+/*click event triggers color change depending what we chose*/
+  map.data.addListener('click', function(event) {
+    event.feature.setProperty("color",Color);
+    map.data.overrideStyle(event.feature, {strokeColor: Color});
+    refreshGeoJsonFromData();
+  });
+  /*right click removes feature*/
+  map.data.addListener('rightclick',function(event){
+    map.data.remove(event.feature);
+  });
+
+
+
+
 
   // Retrieve HTML elements.
   dropContainer = document.getElementById('drop-container');
@@ -34,40 +53,44 @@ function init() {
   var mapContainer = document.getElementById('map-holder');
   geoJsonInput = document.getElementById('geojson-input');
   downloadLink = document.getElementById('download-link');
+    resizeGeoJsonInput();
 
-  // Resize the geoJsonInput textarea.
-  resizeGeoJsonInput();
-
-  // Set up the drag and drop events.
-  // First on common events.
-  [mapContainer, dropContainer].forEach(function(container) {
-    google.maps.event.addDomListener(container, 'drop', handleDrop);
-    google.maps.event.addDomListener(container, 'dragover', showPanel);
-  });
-
-  // Then map-specific events.
-  google.maps.event.addDomListener(mapContainer, 'dragstart', showPanel);
-  google.maps.event.addDomListener(mapContainer, 'dragenter', showPanel);
-
-  // Then the overlay specific events (since it only appears once drag starts).
-  google.maps.event.addDomListener(dropContainer, 'dragend', hidePanel);
-  google.maps.event.addDomListener(dropContainer, 'dragleave', hidePanel);
-  // Set up events for changing the geoJson input.
-  google.maps.event.addDomListener(
-      geoJsonInput,
-      'input',
-      refreshDataFromGeoJson);
-  google.maps.event.addDomListener(
-      geoJsonInput,
-      'input',
-      refreshDownloadLinkFromGeoJson);
-
-  // Set up events for styling.
   google.maps.event.addDomListener(window, 'resize', resizeGeoJsonInput);
 }
 
 
 
+function ShowMenu(control, e) {
+        var posx = e.clientX +window.pageXOffset +'px'; //Left Position of Mouse Pointer
+        var posy = e.clientY + window.pageYOffset + 'px'; //Top Position of Mouse Pointer
+        document.getElementById(control).style.position = 'absolute';
+        document.getElementById(control).style.display = 'inline';
+        document.getElementById(control).style.left = posx;
+        document.getElementById(control).style.top = posy;
+    }
+    function HideMenu(control) {
+
+        document.getElementById(control).style.display = 'none';
+    }
+
+    /*we change a global variable named color depending the button*/
+   function ChangeColor( i){
+     if(i==5)
+       Color="#ffd700";
+     else if (i==4)
+      Color="#006E1F";
+    else if (i==3)
+      Color="#0000FF";
+    else if (i==2)
+      Color="#FFFF66";
+    else if(i==1)
+      Color="#FF3300";
+   }
+
+
+
+google.maps.event.addDomListener(window, 'load', init);
+/*Show geojson button*/
 function showButton() {
         var my_disply = document.getElementById('geojson-input').style.display;
         if(my_disply == "block")
@@ -75,31 +98,10 @@ function showButton() {
         else
               document.getElementById('geojson-input').style.display = "block";
      }
-
-
-
-
-//delete specific element
-
-google.maps.event.addListener(map, 'rightclick', function(event) {
-  mouseLocation = event.latLng;
-  delElement();
-});
-function seDel(){
-  delflag=1;
-}
-function delElement(){
-  map.data.forEach(function(feature) {
-    if(mouseLocation==  map.data.feature.latLng )
-        map.data.remove(feature);
-        
-        });
-}
-
-google.maps.event.addDomListener(window, 'load', init);
-
 //CLear function
 function Clear(){
+
+
   map.data.forEach(function(feature) {
     // If you want, check here for some constraints.
     map.data.remove(feature);
@@ -112,32 +114,6 @@ function refreshGeoJsonFromData() {
     geoJsonInput.value = JSON.stringify(geoJson, null, 2);
     refreshDownloadLinkFromGeoJson();
   });
-}
-
-// Replace the data layer with a new one based on the inputted geoJson.
-function refreshDataFromGeoJson() {
-  var newData = new google.maps.Data({
-    map: map,
-    style: map.data.getStyle(),
-    controls: ['Point', 'LineString', 'Polygon']
-  });
-  try {
-    var userObject = JSON.parse(geoJsonInput.value);
-    var newFeatures = newData.addGeoJson(userObject);
-  } catch (error) {
-    newData.setMap(null);
-    if (geoJsonInput.value !== "") {
-      setGeoJsonValidity(false);
-    } else {
-      setGeoJsonValidity(true);
-    }
-    return;
-  }
-  // No error means GeoJSON was valid!
-  map.data.setMap(null);
-  map.data = newData;
-  bindDataLayerListeners(newData);
-  setGeoJsonValidity(true);
 }
 
 // Refresh download link.
@@ -160,52 +136,6 @@ function setGeoJsonValidity(newVal) {
     geoJsonInput.className = '';
   }
 }
-
-// Control the drag and drop panel. Adapted from this code sample:
-// https://developers.google.com/maps/documentation/javascript/examples/layer-data-dragndrop
-function showPanel(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  dropContainer.className = 'visible';
-  return false;
-}
-
-function hidePanel() {
-  dropContainer.className = '';
-}
-
-function handleDrop(e) {
-  e.preventDefault();
-  e.stopPropagation();
-  hidePanel();
-
-  var files = e.dataTransfer.files;
-  if (files.length) {
-    // process file(s) being dropped
-    // grab the file data from each file
-    for (var i = 0, file; file = files[i]; i++) {
-      var reader = new FileReader();
-      reader.onload = function(e) {
-        map.data.addGeoJson(JSON.parse(e.target.result));
-      };
-      reader.onerror = function(e) {
-        console.error('reading failed');
-      };
-      reader.readAsText(file);
-    }
-  } else {
-    // process non-file (e.g. text or html) content being dropped
-    // grab the plain text version of the data
-    var plainText = e.dataTransfer.getData('text/plain');
-    if (plainText) {
-      map.data.addGeoJson(JSON.parse(plainText));
-    }
-  };
-  // prevent drag event from bubbling further
-  return false;
-}
-
-// Styling related functions.
 function resizeGeoJsonInput() {
   var geoJsonInputRect = geoJsonInput.getBoundingClientRect();
   var panelRect = panel.getBoundingClientRect();
