823902c8dfffabaa60be45e780953d091678cbf3
LefterisIkaria <32582043+LefterisIkaria@users.noreply.github.com>
2018-03-14 16:12:57 +0200

    Add files via upload

--- editor.js
+++ editor.js
@@ -1,7 +1,7 @@
 // The Google Map.
 var map;
 var value = "unknown";
-
+var Window;
 var geoJsonOutput;
 var downloadLink;
 
@@ -16,12 +16,19 @@ function init() {
     mapTypeId: 'satellite'
   });
 	
+  map.data.loadGeoJson("data/2016016.geojson");
+
   map.data.setControls(['Point', 'LineString', 'Polygon']);
   map.data.setStyle({
     editable: true,
     draggable: true,
     clickable: true
   });
+
+  map.data.addListener('rightclick',function(undo){
+    map.data.remove(undo.feature);
+  });
+
   map.data.setStyle(function(feature) {
         var color = "#eee";
         if (feature.getProperty("Rating") == null && feature.getProperty("Color") == null ) {
@@ -39,10 +46,14 @@ function init() {
 
   bindDataLayerListeners(map.data);
 
-  // Retrieve HTML elements.
+// Retrieve HTML elements.
+  Window = document.getElementById('Window');
   var mapContainer = document.getElementById('map-holder');
   geoJsonOutput = document.getElementById('geojson-output');
   downloadLink = document.getElementById('download-link');
+
+  resizeGeoJsonInput();
+  google.maps.event.addDomListener(window, 'resize', resizeGeoJsonInput);
 }
 
 google.maps.event.addDomListener(window, 'load', init);
@@ -50,7 +61,7 @@ google.maps.event.addDomListener(window, 'load', init);
 // Refresh different components from other components.
 function refreshGeoJsonFromData() {
   map.data.toGeoJson(function(geoJson) {
-    geoJsonOutput.value = JSON.stringify(geoJson);
+    geoJsonOutput.value = JSON.stringify(geoJson, null, 2);
     refreshDownloadLinkFromGeoJson();
   });
 }
@@ -65,4 +76,25 @@ function bindDataLayerListeners(dataLayer) {
   dataLayer.addListener('addfeature', refreshGeoJsonFromData);
   dataLayer.addListener('removefeature', refreshGeoJsonFromData);
   dataLayer.addListener('setgeometry', refreshGeoJsonFromData);
+}
+
+//function that shows or hides the geojson output on the screen
+function showButton() {
+        var my_disply = document.getElementById('geojson-output').style.display;
+        if(my_disply == "block")
+              document.getElementById('geojson-output').style.display = "none";
+        else
+              document.getElementById('geojson-output').style.display = "block";
+}
+
+function resizeGeoJsonInput() {
+  var geoJsonOutputRect = geoJsonOutput.getBoundingClientRect();
+  var WindowRect = Window.getBoundingClientRect();
+  geoJsonOutput.style.height = WindowRect.bottom - geoJsonOutputRect.top - 8 + "px";
+}
+
+function DeleteEverythingMwromo(){
+	map.data.forEach(function(feature) {
+        map.data.remove(feature);
+    });
 }
