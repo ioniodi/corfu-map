10af79c9d8ac314c70d6c93cda1c4ebef26d63ef
Orestis Kaplanis <orestismail71@gmail.com>
2018-03-25 20:16:11 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -7,8 +7,8 @@ var downloadLink;
 function init() {
   // Initialise the map.
   map = new google.maps.Map(document.getElementById('map-holder'), {
-    center: {lat: 0, lng: 0},
-    zoom: 3,
+    center: {lat: 39.6249838, lng: 19.922346100000027},
+    zoom: 15,
     mapTypeControl: false,
     streetViewControl: false,
     fullscreenControl: false,
@@ -50,4 +50,19 @@ function bindDataLayerListeners(dataLayer) {
   dataLayer.addListener('addfeature', refreshGeoJsonFromData);
   dataLayer.addListener('removefeature', refreshGeoJsonFromData);
   dataLayer.addListener('setgeometry', refreshGeoJsonFromData);
+}
+// Enable geojson output with the click of the button
+function geojsonOutput() {
+    var show = document.getElementById("geojson-output");
+    if (show.style.display === "block") {
+        show.style.display = "none" ;
+    } else {
+        show.style.display = "block";
+    }
+}
+
+//Delete paths
+function deletepaths(){
+  map.data.forEach(function(e){map.data.remove(e);});
+  geoJsonOutput.value=null;
 }
