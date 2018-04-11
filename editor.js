51fc6fb10471e6f249cc612ef8a1882ef6eeb196
Orestis Kaplanis <orestismail71@gmail.com>
2018-04-11 13:35:38 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -85,7 +85,7 @@ function bindDataLayerListeners(dataLayer) {
     });
 }
 	
-}
+
 // Enable geojson output with the click of the button
 function geojsonOutput() {
     var show = document.getElementById("geojson-output");
