a492a6ec96e56be64f9a94911421013bc56a65a4
gregoriv <gvouterakos@gmail.com>
2018-04-23 02:09:56 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -27,7 +27,13 @@ function init() {
   // Retrieve HTML elements.
   var mapContainer = document.getElementById('map-holder');
   geoJsonOutput = document.getElementById('geojson-output');
+  show_hide_json_btn = document.getElementById('show_hide_geojson');
   downloadLink = document.getElementById('download-link');
+	
+  map.data.addListener('click', function(event) {
+        active_feature = event;
+        add_rating_popup(event);
+    });
 }
 
 google.maps.event.addDomListener(window, 'load', init);
@@ -51,15 +57,33 @@ function bindDataLayerListeners(dataLayer) {
   dataLayer.addListener('removefeature', refreshGeoJsonFromData);
   dataLayer.addListener('setgeometry', refreshGeoJsonFromData);
 }
-function ToggleGeoJsonOutput(){
-  var output = document.getElementById("geojson-output");
-  if (output.style.display === "block"){
-    output.style.display = "none";
-  } else {
-    output.style.display = "block";
-  }
+function add_rating_popup(data) {
+    if(info_window) {
+        info_window.close();
+    }
+    info_window = new google.maps.InfoWindow({
+        content: '<h3>Average Rating: '+get_avg(data.feature.getProperty("ratings")).toFixed(1)+'</h3><h3>Add new rating</h3><select id="new_rating"><option value="1">nope(1)</option><option value="2">meh(2)</option><option value="3">maybe(3)</option><option value="4">ok(4)</option><option value="5">axne(5)</option></select>' +
+        '\t&nbsp;\t&nbsp;<button onclick="add_rating()">Add Rating</button><br><br><br><a onclick="remove_active_feature()" href="#">Remove this</a>',
+        position: data.latLng
+    });
+    info_window.open(map);
+    google.maps.event.addListener(info_window,'closeclick',function(){
+        active_feature = null;
+    });
 }
-function deletepaths(){
-map.data.forEach(function(e){map.data.remove(e);});
-geoJsonOutput.value=null;
+function remove_all_features() {
+    var alert = confirm("Are you sure?");
+    if (alert == true) {
+        map.data.forEach(function(feature) {
+            map.data.remove(feature);
+        });
+        refreshGeoJsonFromData();
+    }
+}
+function remove_active_feature() {
+    if(active_feature) {
+        map.data.remove(active_feature.feature);
+        close_infowindow();
+    }
+    refreshGeoJsonFromData();
 }
