fd96b3cb6c69398e56427d1391b3c783708b4ece
Orestis Kaplanis <orestismail71@gmail.com>
2018-05-04 17:29:24 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -1,14 +1,14 @@
 // The Google Map.
 var map;
-var x;
+
 var geoJsonOutput;
 var downloadLink;
 
 function init() {
   // Initialise the map.
   map = new google.maps.Map(document.getElementById('map-holder'), {
-    center: {lat: 39.6249838, lng: 19.922346100000027},
-    zoom: 15,
+    center: {lat: 0, lng: 0},
+    zoom: 3,
     mapTypeControl: false,
     streetViewControl: false,
     fullscreenControl: false,
@@ -24,25 +24,10 @@ function init() {
 
   bindDataLayerListeners(map.data);
 
-  map.data.addListener('rightclick', function(event){
-        map.data.remove(event.feature);
-    	});
-
   // Retrieve HTML elements.
   var mapContainer = document.getElementById('map-holder');
   geoJsonOutput = document.getElementById('geojson-output');
   downloadLink = document.getElementById('download-link');
-	  //reading back the new color value
-  map.data.setStyle(function(feature) {
-    var default_color = "#000000";
-    if (feature.getProperty("Color")!=x){
-        default_color = feature.getProperty("Color");
-      }
-      else if(feature.getProperty("Color")==null){
-          feature.setProperty("Color", x);
-      }
-      return ({strokeColor: default_color});
-});
 }
 
 google.maps.event.addDomListener(window, 'load', init);
@@ -65,43 +50,4 @@ function bindDataLayerListeners(dataLayer) {
   dataLayer.addListener('addfeature', refreshGeoJsonFromData);
   dataLayer.addListener('removefeature', refreshGeoJsonFromData);
   dataLayer.addListener('setgeometry', refreshGeoJsonFromData);
-	var rating_counter = 1;
-  map.data.addListener('click', function(event) {
-          if(rating_counter == 0)
-             selected_color(event, '#000000');
-          else if(rating_counter == 1)
-            selected_color(event, '#ff0000');
-          else if(rating_counter == 2)
-            selected_color(event, '#ff8100');
-          else if(rating_counter == 3)
-            selected_color(event, '#e3ff00');
-          else if(rating_counter == 4)
-            selected_color(event, '#004c00');
-          else if(rating_counter == 5)
-            selected_color(event, '#00FF00');
-        rating_counter++;
-        if(rating_counter>5)
-          rating_counter = 0;
-    });
-}
-	
-// Enable geojson output with the click of the button
-function geojsonOutput() {
-    var show = document.getElementById("geojson-output");
-    if (show.style.display === "block") {
-        show.style.display = "none" ;
-    } else {
-        show.style.display = "block";
-    }
-}
-
-//Delete paths
-function deletepaths(){
-  map.data.forEach(function(e){map.data.remove(e);});
-  geoJsonOutput.value=null;
-}
-function selected_color(event, x){
-    color = x;
-    map.data.overrideStyle(event.feature,{strokeColor: x});
-    event.feature.setProperty("Color", x);
 }
